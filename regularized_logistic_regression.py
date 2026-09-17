62e61b12ba527afd8f5dc3de9633f93f6cf39754
Wenchao-Zhang66 <1841474470@qq.com>
2026-09-17 22:04:34 +0800

    Refactor learning rate and add sample count output
    Updated learning rate parameter name and added sample count print statements.

--- regularized_logistic_regression.py
+++ regularized_logistic_regression.py
@@ -14,26 +14,19 @@ class SampleData:
         self.x = x
         self.y = y
 
-    
-    """
-    alpha is the learning rate
-    iterations is the number interations
-    lambda is the regularization strength
-    """
-
 
 class LogisticRegression(SampleData):
-    def __init__(self,x,y,alpha=0.01,iterations=10000,lambda_=0.01):
+    def __init__(self,x,y,lr=0.01,iterations=10000,lambda_=0.01):
         super().__init__(x,y)
-        self.alpha=alpha
-        self.iterations=iterations
-        self.lambda_=lambda_
+        self.lr = lr # learning rate
+        self.iterations = iterations # the number of iterations
+        self.lambda_  = lambda_ # the regularization strength
         self.m = self.x.shape[0] # the number of samples
         self.weights = np.zeros((self.x.shape[1], 1)) # initialize parameters
         self.bias = np.zeros((1, 1))
 
     def get_gradient(self):
-        self.z = self.x@self.weights + self.bias
+        self.z = self.x @ self.weights + self.bias
         self.y_pred = np.clip(sigmoid(self.z), 1e-9, 1 - 1e-9) # prediction
         error = self.y_pred - self.y
         dw = (1/self.m) * self.x.T @ error + (self.lambda_/self.m)*self.weights # gradient of the weight
@@ -44,16 +37,16 @@ class LogisticRegression(SampleData):
     def gradient_descent(self):
         for i in range(self.iterations):
             dw, db = self.get_gradient()
-            self.weights -= self.alpha * dw
-            self.bias -= self.alpha * db # gradient descent
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db # gradient descent
 
             if i % 500 == 0:
-                loss = (-1/self.m) * (self.y.T @ np.log(self.y_pred) + (1 - self.y).T @ np.log(1 - self.y_pred))+ (self.lambda_/2)*self.weights.T@self.weights
+                loss = (-1/self.m) * (self.y.T @ np.log(self.y_pred) + (1 - self.y).T @ np.log(1 - self.y_pred))+ (self.lambda_/2)*self.weights.T @ self.weights
                 # "(self.lambda_/2)*self.weights.T@self.weights" is the regularizer
                 print(f"Iteration {i}: Loss = {loss}")
             
             if i == 6000:
-                self.alpha = 0.001 # reduce the learning rate to reduce the step size
+                self.lr = 0.001 # reduce the learning rate to reduce the step size
 
         return self.weights, self.bias, loss
 
@@ -65,7 +58,9 @@ if __name__ == "__main__":
     x = (x-x.mean(axis=0))/(x.std(axis=0)+1e-9) # standardization
     y = bank_marketing.data.targets
     y = y.replace({'no': 0, 'yes': 1}).astype(float) # convert strings to numbers
-    gd = LogisticRegression(x, y, alpha=0.01, iterations=10000, lambda_=0.01)
+    gd = LogisticRegression(x, y, lr=0.01, iterations=10000, lambda_=0.01)
+    print(f"number of samples:{x.shape[0]}")
+    print(f"number of features:{x.shape[1]}")
     print("*******Training started*******")
     optimal_weights, optimal_bias, final_loss = gd.gradient_descent()
     print("*******Training completed*******")
